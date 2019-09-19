8ba6d4f7ce77107e29419d6ec9569374da54087a
Jaime Bisuna <jaimebisona@gmail.com>
2019-09-19 18:56:54 +0800

    First Commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MyInfo(){
+    return (
+        <div>
+            <h1>Jose Jaime Bisuna</h1>
+            <p>
+                Hello friends from the Internet, this is Jaime and I am learning ReactJS.
+            </p>
+            <ul>
+                <li>Boracay</li>
+                <li>New York</li>
+                <li>Beaches</li>
+            </ul>
+        </div>
+    );
+}
+
+
+export default MyInfo
